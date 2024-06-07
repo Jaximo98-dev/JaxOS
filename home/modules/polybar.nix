49d9e2e4c9a38401a73580ff84a4690a500b37b7
jaximo <jaximo98dev@gmail.com>
2024-06-07 13:46:02 +0200

    Polybar 1

--- home/modules/polybar.nix
+++ home/modules/polybar.nix
@@ -4,8 +4,23 @@
   services.polybar = {
     enable = true;
     script = ''
-polybar top &
-polybar bottom &
-'';
+    polybar top &
+    polybar bottom &
+    '';
+  
+    settings = {
+
+      "bar/mybar" = {
+        scroll-up = "#i3.prev";
+        scroll-down = "#i3.next";
+      };
+
+      "module/i3" = {
+        type = internal/i3;
+        ws-icon-0 = "1️⃣";
+      };
+
+
+    };
   };
 }
