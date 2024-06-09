af39418c5349db943508a8161fe9eeaf6edbc125
jaximo <jaximo98dev@gmail.com>
2024-06-09 12:16:13 +0200

    Moar fixes

--- home/modules/xsession.nix
+++ home/modules/xsession.nix
@@ -1,5 +1,8 @@
-{ config, pkgs, ...}:
-
+{ config, pkgs, lib, ...}:
+let
+  # Define the modifier key
+  modifier = config.xsession.windowManager.i3.config.modifier;
+in
 {
   xsession = {
     enable = true;
@@ -11,18 +14,24 @@
         window.titlebar = false;
 
         startup = [
-          {command = "i3-msg 'workspace 1: web; exec firefox'";}
+          {command = "i3-msg 'workspace 1: web; exec firefox; for_window [class=\"Firefox\"] focus'";}
           {command = "i3-msg 'workspace 2: code; exec codium ~/JaxOS'";}     
-          {command = "i3-msg 'workspace 3: tmux; exec tmux'";}
+          {command = "i3-msg 'workspace 3: tmux; exec alacritty -e tmux'";}
           {command = "i3-msg 'workspace 5: ds; exec discord'";}  
         ];
 
         assigns = {
-          "1: web" = [ { class = "^Firefox$"; } ];
-          "2: code" = [ { class = "^VSCodium$"; } ]; 
-          "3: tmux" = [ { class = "^Tmux$"; } ];
-          "5: ds" = [ { class = "^Discord$"; } ];
-      };
+          "1: web" = [ { class = "Firefox"; } ];
+          "2: code" = [ { class = "VSCodium"; } ]; 
+          "3: tmux" = [ { class = "Alacritty"; instance = "tmux"; } ];
+          "5: ds" = [ { class = "Discord";} ];
+        };
+        
+        keybindings = lib.mkOptionDefault {
+          "${modifier}+Return" = "exec alacritty";
+          "${modifier}+Shift+q" = "kill";
+          "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi";
+        };
 
       };
     };
