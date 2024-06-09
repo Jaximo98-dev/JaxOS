fd10c3fe31029c028daa0e3d1818b1c0610d3ec7
jaximo <jaximo98dev@gmail.com>
2024-06-09 13:51:49 +0200

    Screen monitor, and NO keybindings, Introduction to laptop todo

--- home/modules/xsession.nix
+++ home/modules/xsession.nix
@@ -6,6 +6,7 @@ in
 {
   xsession = {
     enable = true;
+    initExtra = " xrandr --output DP-3 --mode 2560x1440 --rate 144"; #TODO portatil
     windowManager.i3 = {
       enable = true;
       config = {
@@ -14,16 +15,16 @@ in
         window.titlebar = false;
 
         startup = [
-          {command = "i3-msg 'workspace 1: web; exec firefox; for_window [class=\"Firefox\"] focus'";}
-          {command = "i3-msg 'workspace 2: code; exec codium ~/JaxOS'";}     
+          {command = "i3-msg 'workspace 1: web; exec firefox; for_window [class=\"Firefox\"] move to workspace 1'";}
+          {command = "i3-msg 'workspace 2: code; exec codium ~/JaxOS;for_window [class=\"VSCodium\"] move to workspace 2 '";}     
           {command = "i3-msg 'workspace 5: ds; exec discord; for_window [class=\"discord\"] move to workspace 5'";} 
-          {command = "i3-msg 'workspace 3: tmux; exec alacritty -e tmux; for_window [instance=\"tmux\"] move to workspace 3'";}
+          {command = "i3-msg 'workspace 3: console; exec alacritty -e tmux; for_window [instance=\"tmux\"] move to workspace 3'";}
         ];
 
         assigns = {
           "1: web" = [ { class = "Firefox"; } ];
           "2: code" = [ { class = "VSCodium"; } ]; 
-          "3: tmux" = [ { class = "Alacritty"; } ];
+          "3: console" = [ { class = "Alacritty"; } ];
           "5: ds" = [ { class = "discord"; instance = "discord"; } ];
         };
         
@@ -31,8 +32,8 @@ in
           "${modifier}+Return" = "exec alacritty";
           "${modifier}+Shift+q" = "kill";
           "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
-          "button7" = "workspace prev"; # Rueda raton izq
-          "button6" = "workspace next"; # Rueda raton der
+          #"button7" = "workspace prev"; # Rueda raton izq
+          #"button6" = "workspace next"; # Rueda raton der
         };
 
       };
