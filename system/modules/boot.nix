33a771fa94eea44a02ab9f8951fdcd637f65dfeb
jaximo <jaximo98dev@gmail.com>
2024-06-15 16:05:01 +0200

    Grub updated, with theme

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -13,5 +13,20 @@
 		#gfxmodeEfi = "2560x1440"; #TODO: Hacerlo adaptativo
 		fontSize = 42;
 		configurationLimit = 20;
+		extraEntriesBeforeNixOS = true;
+		theme = pkgs.stdenv.mkDerivation {
+				pname = "grub_bios_theme";
+				version = "1";
+				src = pkgs.fetchFromGitHub {
+					owner = "Blaysht";
+					repo = "grub_bios_theme";
+					rev = "035554c30df6a10158a5a71acfbc4975045fc7ac";
+					hash = "sha256-kYcEMCV9ipwPGgfAwOtFgYO4eHZxkUS97tOr0ft4rUE=";
+					};
+ 			  installPhase = ''
+            mkdir -p $out/share/grub/themes/CRT-Amber
+            cp -r * $out/share/grub/themes/CRT-Amber
+        '';
+			};
 	};
 }
