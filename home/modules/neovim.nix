65893b63a17e662832ec0542fa6892fcce3c44d0
jaximo <jaximo98dev@gmail.com>
2024-07-21 00:35:09 +0200

    Neovim configs

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -1,7 +1,15 @@
-{ config, pkgs, ...}:
+{ config, pkgs, lib, ...}:
 
 {
   programs.neovim = {
     enable = true;
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraLuaConfig = builtins.readFile(./nvim/init.lua);
+
+    plugins = with pkgs.vimPlugins
   };
 }
