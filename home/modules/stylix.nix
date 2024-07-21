b8dd201864c0c539cf83a343dc42b420d45fd5af
jaximo <jaximo98dev@gmail.com>
2024-07-21 21:17:47 +0200

    ...

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -5,6 +5,6 @@
   }];
 
   stylix.enable = true;
-  stylix.image = ../stylix/gruv-greek.png;
+  stylix.image = ./stylix/gruv-greek.png;
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
 }
