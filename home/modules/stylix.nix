aec31786f53a14dd8383ee50bb21e94cfcb489b5
jaximo <jaximo98dev@gmail.com>
2024-07-21 21:21:53 +0200

    More stylix

--- home/modules/stylix.nix
+++ home/modules/stylix.nix
@@ -1,9 +1,5 @@
 { pkgs, ...}:
 {
-  home-manager.sharedModules = [{
-    stylix.targets.xyz.enable = false;
-  }];
-
   stylix.enable = true;
   stylix.image = ./stylix/gruv-greek.png;
   #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
