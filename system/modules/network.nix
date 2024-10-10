4e53c8c44092dc4faffe6c37dd17f2129fc0a6a8
jaximo <jaximo98dev@gmail.com>
2024-10-10 12:07:18 +0200

    Comments

--- system/modules/network.nix
+++ system/modules/network.nix
@@ -7,6 +7,7 @@
   networking.networkmanager.enable = true;
   networking.hostName = "Jaximo_dev";
 
+  # Try to set up connection to eduroam, expected to run the installer original eduroam script first to generate wpa_supplicant.
   systemd.services.connect-eduroam = lib.mkIf (device == "laptop") {
     description = "Connect to EDUROAM WiFi network";
     after = [ "network.target" ];
