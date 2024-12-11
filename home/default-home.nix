d1e1c8d16015b709effd97b4ff22417b9bb09e85
jaximo_dev <jaximo98dev@gmail.com>
2024-12-11 22:07:07 +0100

    Cheat to navi

--- home/default-home.nix
+++ home/default-home.nix
@@ -16,7 +16,7 @@
 
     ./modules/zsh.nix           # Shell interpreter
     ./modules/shell_tools.nix
-    ./modules/cheat.nix
+    ./modules/navi.nix
     ./modules/git.nix           # git
     ./modules/btop.nix          # Performance monitor
 
---
+++ home/modules/navi.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ...}:
+{
+  programs.navi = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+      cheats.path = [ "$JAXOS/cheatsheets" ];
+      client.tealdeer = true;
+      shell.command = "zsh";
+  };
+ }
+}
